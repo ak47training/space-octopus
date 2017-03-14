072573b34613c54aec0b12948fa748f3929335ff
Leo G <ak47training@gmail.com>
2017-03-14 03:36:39 +0300

    tt2

--- deck.rb
+++ deck.rb
@@ -1,6 +1,28 @@
 require 'squib'
 
-Squib::Deck.new(cards: 3, layout: 'layout.yml') do
-  text str: 'Hello, World!'
-  save format: :png
+data = Squib.xlsx file: 'Ships.xlsx'
+
+Squib::Deck.new cards: data['Name'].size, layout: ['economy.yml', 'layout.yml'] do
+  /data = csv file: 'Ships.csv'/
+  background color: 'white'
+  rect layout: 'cut'
+  rect layout: 'safe'
+  text str: data['Name'], layout: 'title'
+  text str: data['Text'], layout: 'description'
+  text str: Time.now, layout: 'credits'
+  svg layout: data['Type']
+  
+  save_png
+  save_pdf trim: 37.5
 end
+
+/Squib::Deck.new cards: data['Name'].size, layout: 'economy.yml' do
+  background color: 'white'
+  rect layout: 'cut'
+  rect layout: 'safe'
+  text str: data['Name'], layout: 'title'
+  text str: data['Text'], layout: 'description'
+  text str: Time.now, layout: 'credits'
+  save_png
+  save_pdf trim: 37.5
+end  /
