d4b5a71b1b21b26ec4347276c7d3c446676c55ab
Leo G <ak47training@gmail.com>
2017-03-20 01:56:15 +0300

    cost icons function properly

--- deck.rb
+++ deck.rb
@@ -1,22 +1,24 @@
 require 'squib'
 
 data = Squib.xlsx file: 'Tactics.xlsx'
+cost_icon_width = 50
+cost_icon_height = 50
 
 Squib::Deck.new cards: data['Name'].size, layout: ['layout.yml'] do
-  /data = csv file: 'Ships.csv'/
   background color: 'white'
   hint text: "#333"
   rect layout: 'cut'
   rect layout: 'safe'
   titlesizes = data['Name'].map do |name|
     case name.length
-    when 0..7 then 64
-    when 8..9 then 50
-    when 10..50 then 45
+    when 0..7 then 32
+    when 8..9 then 24
+    when 10..50 then 18
     end
+  end
   text str: data['Name'], layout: :Title, font_size: titlesizes
   text str: data['Text'], layout: 'Ability'
-  /text str: data['Cost'], layout: 'upper_right'/
+
   
   text str: data['Bonus'], layout: 'BonusAbility'
   text str: data['Picket bonus'], layout: 'BonusPicket'
@@ -26,18 +28,18 @@ Squib::Deck.new cards: data['Name'].size, layout: ['layout.yml'] do
   text str: Time.now, layout: 'credits'
   
   #Cost icons in top right:
-  dice_cost = data['Cost'].map do |c|
-    case c 
-    when 'A' then 'icons\attack.svg'
-    when 'D' then 'icons\attack.svg'
-    when 'S' then 'icons\speed.svg'
-    when 'U' then 'icons\utility.svg'
-    when 'I' then 'icons\intel.svg'
-    when 'L' then 'icons\leadership.svg'
-    end
+  text(str: data['Cost'], layout: 'upper_right') do |embed|
+    embed.svg key: '(ATK)', width: cost_icon_width, height: cost_icon_height, file: 'icons\attack.svg'
+	embed.svg key: '(DEF)', width: cost_icon_width, height: cost_icon_height, file: 'icons\defend.svg'
+	embed.svg key: '(SPD)', width: cost_icon_width, height: cost_icon_height, file: 'icons\speed.svg'
+	embed.svg key: '(INT)', width: cost_icon_width, height: cost_icon_height, file: 'icons\intel.svg'
+	embed.svg key: '(LDR)', width: cost_icon_width, height: cost_icon_height, file: 'icons\leadership.svg'
+	embed.svg key: '(UTL)', width: cost_icon_width, height: cost_icon_height, file: 'icons\utility.svg'
   end
-  svg file: dice_cost, layout: 'cost1'
-end
+	
+
+
+
     
   save_png
   save_pdf trim: 37.5
