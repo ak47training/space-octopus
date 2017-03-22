d976754ef42b258d0cfd34c598c60d87354c1928
Leo G <ak47training@gmail.com>
2017-03-22 17:02:00 +0300

    ships providing dice done, atk-spd-def halfdone

--- ships.rb
+++ ships.rb
@@ -27,6 +27,10 @@ Squib::Deck.new cards: data['Name'].size, layout: ['ships_layout.yml', 'layout.y
 	  end
 	end
   end
+  ranges = data['Range'].map do |rangefy|
+    'Range: ' + rangefy
+  end
+  
   textsizes = data['Text2'].map do |name2|
     unless name2 == nil then
 	  case name2.length
@@ -38,7 +42,7 @@ Squib::Deck.new cards: data['Name'].size, layout: ['ships_layout.yml', 'layout.y
   end
   text str: data['Name'], layout: :Title, font_size: titlesizes
   
-  text str: data['Range'], ellipsize: false, layout: 'StatRange'
+  text str: ranges, ellipsize: false, layout: 'StatRange'
   text str: data['Type keywords'], layout: 'Type'
   
   text str: data['Text2'],  ellipsize: false, layout: 'TextAbility', font_size: textsizes
@@ -63,6 +67,19 @@ Squib::Deck.new cards: data['Name'].size, layout: ['ships_layout.yml', 'layout.y
     embed.svg key: '(ANY)', width: cost_icon_width, height: cost_icon_height, file: 'icons\anydie.svg'
   end
  
+  text(str: data['Provides'], layout: 'Provides', angle: Math::PI / 2) do |embed|
+    embed.svg key: '(ATK)', width: cost_icon_width, height: cost_icon_height, angle: Math::PI*3/2, file: 'icons\attack.svg'
+	embed.svg key: '(DEF)', width: cost_icon_width, height: cost_icon_height, angle: Math::PI*3/2, file: 'icons\defend.svg'
+	embed.svg key: '(SPD)', width: cost_icon_width, height: cost_icon_height, angle: Math::PI*3/2, file: 'icons\speed.svg'
+	embed.svg key: '(INT)', width: cost_icon_width, height: cost_icon_height, angle: Math::PI*3/2, file: 'icons\intel.svg'
+	embed.svg key: '(LDR)', width: cost_icon_width, height: cost_icon_height, angle: Math::PI*3/2, file: 'icons\leadership.svg'
+	embed.svg key: '(UTL)', width: cost_icon_width, height: cost_icon_height, angle: Math::PI*3/2, file: 'icons\utility.svg'
+    embed.svg key: '(ANY)', width: cost_icon_width, height: cost_icon_height, angle: Math::PI*3/2, file: 'icons\anydie.svg'
+  end
+  
+  text str: data['ATK'], layout: 'lower_left'
+  text str: data['SPD'], layout: 'lower_right'
+  text str: data['DEF'], layout: 'next_to_lower_right'
 
 
 
